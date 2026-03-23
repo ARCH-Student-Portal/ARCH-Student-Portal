91f909ecf578487655ca37f7497683560ae53601
ItzRayCart <rayesh1922@gmail.com>
2026-03-23 22:46:13 +0500

    Added Routes/Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
 import StudentDashV1 from './StudentDashV1';
+import LoginPage from './LoginPage';
 function App() {
-  return <StudentDashV1 />;
+  return ( <Routes>
+      <Route path="/" element={<LoginPage/>} />
+      <Route path="/dashboard" element={<StudentDashV1/>} />
+      <Route path="*" element={<Navigate to="/" replace/>} />
+    </Routes>
+  );
 }
 export default App;
